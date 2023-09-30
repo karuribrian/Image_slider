5997eae3ffd9988a78e33f6cb3f338c45d410067
karuribrian <brianmwenda418@gmail.com>
2023-09-30 21:18:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const slider = document.querySelector(".slider_container")
+    const pictures = document.querySelectorAll(".slides img")
+});
